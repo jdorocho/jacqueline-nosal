bab70782e7b21fd52039d4ff45cae2e74cccd9de
Jacqui Nosal <jacqueline.dchow@gmail.com>
2020-03-19 12:59:34 -0400

    cleaned up comments in scripts files

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,18 +1,16 @@
-// Typed.js by www.mattboldt.com 
+// Typed.js
 const typed = new Typed('#helloThere', {
     strings: ["Hello there! Click here or scroll down to learn more."],
     typeSpeed: 65
 });
 
-// "Jacqui" animation in header from Wes Bos' JavaScript 30 
+// "Jacqui" animation in header from Wes Bos' JavaScript30 
 const mainHeader = document.querySelector('.mainHeader');
 const text = mainHeader.querySelector('h1');
 const walk = 350; 
 
 function shadow(e) {
-    // const width = mainHeader.offsetWidth;
-    // const height = mainHeader.offsetHeight; 
-    const {offsetWidth: width, offsetHeight: height} = mainHeader; // Destructured version
+    const {offsetWidth: width, offsetHeight: height} = mainHeader; 
 
     let {offsetX: x, offsetY: y} = e;
 
@@ -37,7 +35,7 @@ mainHeader.addEventListener('mousemove', shadow);
 $(function() {
     console.log("Hi there! Thanks for taking the time to visit my site. :) I hope you have a wonderful day, and I look forward to connecting with you soon! - Jacqui");
 
-    // smooth scroll https://github.com/kswedberg/jquery-smooth-scroll 
+    // smooth scroll 
     $('a').smoothScroll({
         speed: 700
     });
@@ -56,27 +54,23 @@ $(function() {
     });
 });
 
-// AOS action
+// AOS (Animate On Scroll)
 AOS.init();
-
 AOS.init({
-    // Global settings:
-    disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
-    startEvent: 'DOMContentLoaded', // name of the event dispatched on the document, that AOS should initialize on
-    initClassName: 'aos-init', // class applied after initialization
-    animatedClassName: 'aos-animate', // class applied on animation
-    useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
-    disableMutationObserver: false, // disables automatic mutations' detections (advanced)
-    debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
-    throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
+    disable: false, 
+    startEvent: 'DOMContentLoaded', 
+    initClassName: 'aos-init', 
+    animatedClassName: 'aos-animate', 
+    useClassNames: false, 
+    disableMutationObserver: false, 
+    debounceDelay: 50, 
+    throttleDelay: 99, 
     
-
-    // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
-    offset: 120, // offset (in px) from the original trigger point
-    delay: 0, // values from 0 to 3000, with step 50ms
-    duration: 400, // values from 0 to 3000, with step 50ms
-    easing: 'ease', // default easing for AOS animations
-    once: false, // whether animation should happen only once - while scrolling down
-    mirror: false, // whether elements should animate out while scrolling past them
-    anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
+    offset: 120, 
+    delay: 0, 
+    duration: 400, 
+    easing: 'ease', 
+    once: false, 
+    mirror: false, 
+    anchorPlacement: 'top-bottom', 
 });
